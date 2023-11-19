64ecca8e9370c07b83d4f19358c5ac969150c9c9
ahmad.albaqir <ahmad.albaqir@gojek.com>
2023-11-19 13:42:39 +0700

    implement  build grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Identicon do
     input
     |> hash
     |> pick_color
+	  |> build_grid
   end
 
   def hash(input) do
@@ -14,5 +15,10 @@ defmodule Identicon do
 
   def pick_color(%Identicon.Image{hex:  [red, green, blue | _]} = image) do
     %Identicon.Image{image | color: {red, green, blue}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = _) do
+    hex
+    |> Enum.chunk_every(3)
   end
 end 
